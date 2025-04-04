fe2c95b08f2b483405615109300bac45efcde300
geonwoo <bokko7711@naver.com>
2025-04-05 00:58:32 +0900

    aws 배포 중 디버깅 : 아

--- scripts/start.sh
+++ scripts/start.sh
@@ -20,12 +20,7 @@ set +a
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-source ~/.bashrc
-nohup java -jar $JAR_FILE \
-  --spring.datasource.url=$DB_URL \
-  --spring.datasource.username=$DB_USERNAME \
-  --spring.datasource.password=$DB_PASSWORD \
-  > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG

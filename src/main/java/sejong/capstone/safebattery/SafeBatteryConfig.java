298309d8d0393693b1d5f89fb7630a5a545a22fc
geonwoo <bokko7711@naver.com>
2025-04-30 21:59:43 +0900

    CORS config bean 추가 - 기존 설정 파일에 추가하는 방식으로 리팩토링

--- src/main/java/sejong/capstone/safebattery/SafeBatteryConfig.java
+++ src/main/java/sejong/capstone/safebattery/SafeBatteryConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import sejong.capstone.safebattery.repository.ClientRepository;
 import sejong.capstone.safebattery.repository.PemfcRepository;
 import sejong.capstone.safebattery.repository.PredictionRepository;
@@ -16,6 +18,20 @@ public class SafeBatteryConfig {
 
     @Value("${ai.server.url}")
     private String aiServerUrl;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
+                        .allowedOrigins("http://localhost:3000")  // React 앱 주소
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
+                        .allowedHeaders("*")  // 모든 헤더 허용
+                        .allowCredentials(true);  // 인증이 필요한 요청 허용
+            }
+        };
+    }
 
     @Bean
     public WebClient webClient() {

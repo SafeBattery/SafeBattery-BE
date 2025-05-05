7f1e94e2f60da2bb91f9ce83508ee4643cb99c23
10cheon00 <10cheon00@gmail.com>
2025-05-05 22:59:19 +0900

    fix: constructor already exist error

--- src/main/java/sejong/capstone/safebattery/domain/PowerPrediction.java
+++ src/main/java/sejong/capstone/safebattery/domain/PowerPrediction.java
@@ -1,14 +1,12 @@
 package sejong.capstone.safebattery.domain;
 
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @SuperBuilder
 @NoArgsConstructor
-@AllArgsConstructor
 public class PowerPrediction extends BasePrediction {
 
 }
--- src/main/java/sejong/capstone/safebattery/domain/VoltagePrediction.java
+++ src/main/java/sejong/capstone/safebattery/domain/VoltagePrediction.java
@@ -1,14 +1,14 @@
 package sejong.capstone.safebattery.domain;
 
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Entity
+@Getter
 @SuperBuilder
 @NoArgsConstructor
-@AllArgsConstructor
 public class VoltagePrediction extends BasePrediction {
 
 }

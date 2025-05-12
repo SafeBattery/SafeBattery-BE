494acff238169b3cffb8e2542d7fbdb9a2c7908f
geonwoo <bokko7711@naver.com>
2025-05-12 20:41:51 +0900

    예외발생 시 처리 로직 - 로그 추가

--- src/main/java/sejong/capstone/safebattery/exception/ServerExceptionHandler.java
+++ src/main/java/sejong/capstone/safebattery/exception/ServerExceptionHandler.java
@@ -12,12 +12,14 @@ public class ServerExceptionHandler {
 
     @ExceptionHandler(AiServerException.class)
     public ResponseEntity<String> handleAIServerException(AiServerException e) {
+        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_GATEWAY)
                 .body("AI 서버와 통신 중 에러 발생 : " + e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleOtherExceptions(Exception e) {
+        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("서버 오류 발생");
     }

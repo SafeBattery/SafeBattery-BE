5394ed61286382338fb54ac957a5ce1277b7aef4
geonwoo <bokko7711@naver.com>
2025-05-16 16:37:30 +0900

    Datainit 수정 : 두 pemfc 객체를 같게 설정

--- src/main/java/sejong/capstone/safebattery/DataInit.java
+++ src/main/java/sejong/capstone/safebattery/DataInit.java
@@ -37,13 +37,16 @@ public class DataInit {
             System.setOut(new PrintStream(OutputStream.nullOutputStream())); // 출력 끔
 
             Client client = new Client("Gildong Hong");
-            Pemfc pemfc1 = new Pemfc(client, NORMAL, NORMAL, NORMAL, 34, 127, "testPemfc-001", LocalDate.of(2025, 1, 1));
-            Pemfc pemfc2 = new Pemfc(client, NORMAL, NORMAL, NORMAL, 34, 127, "testPemfc-001", LocalDate.of(2025, 2, 2));
-            Pemfc pemfc3 = new Pemfc(client, NORMAL, NORMAL, NORMAL, 34, 127, "testPemfc-001", LocalDate.of(2025, 3, 3));
-            clientRepository.save(client);
-            pemfcRepository.save(pemfc1);
-            pemfcRepository.save(pemfc2);
-            pemfcRepository.save(pemfc3);
+//            Pemfc pemfc1 = new Pemfc(client, NORMAL, NORMAL, NORMAL, 34, 127, "testPemfc-001", LocalDate.of(2025, 1, 1));
+//            Pemfc pemfc2 = new Pemfc(client, NORMAL, NORMAL, NORMAL, 34, 127, "testPemfc-001", LocalDate.of(2025, 2, 2));
+//            Pemfc pemfc3 = new Pemfc(client, NORMAL, NORMAL, NORMAL, 34, 127, "testPemfc-001", LocalDate.of(2025, 3, 3));
+//            clientRepository.save(client);
+//            pemfcRepository.save(pemfc1);
+//            pemfcRepository.save(pemfc2);
+//            pemfcRepository.save(pemfc3);
+            Pemfc pemfc1 = pemfcRepository.findById(1L).orElseThrow();
+            Pemfc pemfc2 = pemfcRepository.findById(2L).orElseThrow();
+            Pemfc pemfc3 = pemfcRepository.findById(3L).orElseThrow();
 
             recordService.add600RowsFromCsv(pemfc1.getId());
             recordService.add600RowsFromCsv(pemfc2.getId());
